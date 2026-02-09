01594786b53f8597f8eb01f2ad05ed1c96fca4d4
thaodinh97 <23521465@gm.uit.edu.vn>
2026-02-09 22:18:55 +0700

    fix: disable compute config

--- infrastructure/eks.tf
+++ infrastructure/eks.tf
@@ -8,6 +8,9 @@ module "eks" {
   endpoint_public_access                   = true
   enable_cluster_creator_admin_permissions = true
 
+  compute_config = {
+    enable = false
+  }
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
