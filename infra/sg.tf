803103c4980735270e562d1341c83470ce5c3143
thaodinh97 <23521465@gm.uit.edu.vn>
2026-02-23 23:51:01 +0700

    fix(infra): format

--- infra/sg.tf
+++ infra/sg.tf
@@ -11,9 +11,9 @@ resource "aws_security_group" "alb" {
   }
 
   ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
